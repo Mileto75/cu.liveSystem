c096b54d6891f01ee3940d5d48d1501bd8aaaff0
Mileto Di Marco <mileto.di.marco@howest.be>
2025-11-20 14:32:47 +0100

    Add quotecomponent

---
+++ cu.liveSystem.blazor/Models/QuoteModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace cu.liveSystem.blazor.Models
+{
+    public class QuoteModel
+    {
+        [JsonPropertyName("icon_url")]
+        public string IconUrl { get; set; }
+        [JsonPropertyName("value")]
+        public string Value { get; set; }
+    }
+}
--- cu.liveSystem.blazor/Services/StatusService.cs
+++ cu.liveSystem.blazor/Services/StatusService.cs
@@ -1,6 +1,8 @@
 ﻿
 using cu.liveSystem.blazor.Hubs;
+using cu.liveSystem.blazor.Models;
 using Microsoft.AspNetCore.SignalR;
+using System.Text.Json;
 
 namespace cu.liveSystem.blazor.Services
 {
@@ -8,10 +10,14 @@ namespace cu.liveSystem.blazor.Services
     {
         //reference to hub
         private readonly IHubContext<StatusHub> _hubContext;
+        private readonly Uri _uri;
+        private readonly HttpClient _httpClient;
 
-        public StatusService(IHubContext<StatusHub> hubContext)
+        public StatusService(IHubContext<StatusHub> hubContext, HttpClient httpClient)
         {
             _hubContext = hubContext;
+            _uri = new Uri("https://api.chucknorris.io/jokes/random");
+            _httpClient = httpClient;
         }
 
         protected async override Task ExecuteAsync(CancellationToken stoppingToken)
@@ -21,8 +27,16 @@ namespace cu.liveSystem.blazor.Services
                 var guid = Guid.NewGuid();
                 //use hub to broadcast
                 await _hubContext.Clients.All.SendAsync("ReceiveStatusUpdate", "system", guid);
+                await _hubContext.Clients.All
+                    .SendAsync("ReceiveQuote", await GetQuote());
                 await Task.Delay(3000);//wait three seconds
             }
+        }
+        protected async Task<QuoteModel> GetQuote()
+        {
+            var result = await _httpClient.GetAsync(_uri.AbsoluteUri);
+            var content = await result.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<QuoteModel>(content);
         }
     }
 }
